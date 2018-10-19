2ee9fe64fdd884542726ad0d1c403ffb332df17d
Mike-415 <mgonz160@mail.ccsf.edu>
2018-10-18 17:26:17 -0700

    Added comments to each method and cleaned up the code

--- main.cpp
+++ main.cpp
@@ -42,15 +42,47 @@ class Fraction {
 
 
 
+
+
+
+/**
+ * Initializes or reinitializes the data members
+ *
+ * @param inNumerator represents the numerator
+ * @param inDenominator represents the denominator
+ */
 void Fraction::set(int inNumerator, int inDenominator) {
     numerator = inNumerator;
     denominator = inDenominator;
 }
 
+
+
+
+
+
+/**
+ * Formats and displays the data members to the console
+ */
 void Fraction::print() {
     cout << numerator << "/" << denominator;
 }
 
+
+
+
+
+
+/**
+ * A helper method for either the addition or subtraction operations
+ *
+ * @param isAddition represents if the client wants an addition operation
+ *        if false, perform a subtraction operation
+ * @param otherFraction represents the other Fraction object
+ *
+ * @return a new Fraction object that represents the results of the
+ *         arithmetic operation
+ */
 Fraction Fraction::addOrSubtract(bool isAddition, Fraction otherFraction) {
     Fraction temp;
     int crossMultiply1 = numerator * otherFraction.denominator;
@@ -62,14 +94,47 @@ Fraction Fraction::addOrSubtract(bool isAddition, Fraction otherFraction) {
     return temp;
 }
 
+
+
+
+
+
+/**
+ * Adds this Fraction to another Fraction
+ * @param otherFraction represents the other Fraction object
+ * @return a new Fraction object that is the sum of this Fraction and
+ *         the other Fraction
+ */
 Fraction Fraction::addedTo(Fraction otherFraction) {
     return addOrSubtract(true, otherFraction);
 }
 
+
+
+
+
+
+/**
+ * Subtracts this Fraction to another Fraction
+ * @param otherFraction represents the other Fraction object
+ * @return a new Fraction object that is the difference of this Fraction and
+ *         the other Fraction
+ */
 Fraction Fraction::subtract(Fraction otherFraction) {
     return addOrSubtract(false, otherFraction);
 }
 
+
+
+
+
+
+/**
+ * Multiplies this Fraction to another Fraction
+ * @param otherFraction represents the other Fraction object
+ * @return a new Fraction object that is the product of this Fraction and
+ *         the other Fraction
+ */
 Fraction Fraction::multipliedBy(Fraction otherFraction) {
     Fraction temp;
     temp.set((numerator * otherFraction.numerator),
@@ -77,6 +142,17 @@ Fraction Fraction::multipliedBy(Fraction otherFraction) {
     return temp;
 }
 
+
+
+
+
+
+/**
+ * Divides this Fraction to another Fraction
+ * @param otherFraction represents the other Fraction object
+ * @return a new Fraction object that is the quotient of this Fraction and
+ *         the other Fraction
+ */
 Fraction Fraction::dividedBy(Fraction otherFraction) {
     Fraction temp;
     temp.set((numerator * otherFraction.denominator),
@@ -84,10 +160,24 @@ Fraction Fraction::dividedBy(Fraction otherFraction) {
     return temp;
 }
 
+
+
+
+
+
+/**
+ * Determines if this Fraction and another Fraction are equal
+ * @param otherFraction represents the other Fraction object
+ * @return true if both Fractions are equal, false otherwise
+ */
 bool Fraction::isEqualTo(Fraction otherFraction) {
     return (numerator * otherFraction.denominator ) ==
            (denominator * otherFraction.numerator);
 }
+
+
+
+
 
 
 int main() {
@@ -144,98 +234,11 @@ int main() {
 /*
 OUTPUT:
 
- The product of 9/8 and 2/3 is 18/24
+The product of 9/8 and 2/3 is 18/24
 The quotient of 9/8 and 2/3 is 27/16
 The sum of 9/8 and 2/3 is 43/24
 The difference of 9/8 and 2/3 is 11/24
 The two Fractions are not equal.
-
-Write a Fraction class whose objects will represent Fractions.
-
-Note: this is the first part of a multi-part assignment.
- For this week you should not simplify (reduce) fractions,
- you should not use "const,"
- and all of your code should be in a single file.
-
-In this single file, the
- class declaration will come first,
- followed by the definitions of the class member functions,
- followed by the client program.
-
-You must provide the following member functions:
-
-A set() operation that takes two integer arguments,
- a numerator and
- a denominator,
- and sets the calling object accordingly.
-
-Arithmetic operations that
- add,
- subtract,
- multiply, and
- divide Fractions.
- These should be implemented as value returning functions
- that return a Fraction object. They should be named
- addedTo,
- subtract,
- multipliedBy, and
- dividedBy.
- In these functions you will need to declare a local "Fraction" variable,
- assign to it the result of the mathematical operation, and then return it.
-
-A boolean operation named isEqualTo that
- compares two Fraction objects for equality.
- Since you aren't reducing your Fractions,
- you'll need to do this by cross-multiplying.
- A little review:
- if numerator1 * denominator2 equals denominator1 * numerator2,
- then the Fractions are equal.
-
-An output operation named print that displays the value of a Fraction object
- on the screen in the form numerator/denominator.
-
-Your class should have exactly two data members,
- one to represent the numerator of the Fraction being represented,
- and one to represent the denominator of the Fraction being represented.
-
-Here's a hint for how you will set up your arithmetic operation functions:
- You need two Fractions.
-    One is the parameter,
-    one is the calling object.
- The function multiplies the calling object times
- the parameter and returns the result.
- In some ways it is similar to the comesBefore() function from the lesson.
- That function also needs two Fractions,
- and one is the calling object and one is the parameter.
-
-When adding or subtracting Fractions,
- remember that you must first find the common denominator.
- The easy way to do this is to multiply the denominators together
- and use that product as the common denominator.
-
-I am providing a client program for you below.
- You should copy and paste this and use it as your client program.
- The output that should be produced when the provided client program
- is run with your class is also given below, so that you can check your results.
-
-I strongly suggest that you design your class incrementally.
- For example, you should first implement only the set function
- and the output function,
- and then test what you have so far.
- Once this code has been thoroughly debugged,
- you should add additional member functions,
- testing each one thoroughly as it is added.
- You might do this by creating your own client program to test the code
- at each stage; however, it would probably be better
- to use the provided client program and comment out code
- that relates to member functions that you have not yet implemented.
-
-As you can see from the sample output given below,
- you are not required to reduce Fractions or
- change improper Fractions into mixed numbers for printing.
- Just print it as an improper Fraction.
- You are also not required to deal with negative numbers,
- either in the numerator or the denominator.
 
  */
 
